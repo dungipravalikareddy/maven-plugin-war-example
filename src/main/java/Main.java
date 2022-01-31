5026bca5672e0047acbf38f31648f380a0651940
dungipravalikareddy <89082767+dungipravalikareddy@users.noreply.github.com>
2022-01-31 16:40:15 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import org.eclipse.jetty.servlet.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+@WebServlet("/Main")
 public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
